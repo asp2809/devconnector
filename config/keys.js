ba9780a58b41746de844d608214d8bace7689d6c
Anshu S Panda <anshuspanda@gmail.com>
2019-09-16 01:23:46 +0530

    ✏Keys Updated

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://anshu:papajio1@ds161104.mlab.com:61104/developerconnect",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
